85ab57331a66c72c73e1f4cc8656b3a540ddcfba
Alex Markin <alexndr86@gmail.com>
2021-10-20 18:39:35 +0300

    Add basic paths for blog

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"data": {"name": "Alex"}}
+@app.get("/blog")
+def index(limit=10, published: bool = True):
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} from the db"}
 
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get("/blog/unpublished")
+def unpublished():
+    # fetch blog with id
+    return {"data": "All unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def about(id: int, limit=10):
+    # fetch blog with id
+    return {"data": {"comments": ["Comment 1", "Comment 2", "Comment 3"]}}
+
+
+@app.post('/blog')
+def create_blog():
+    return {"data": "blog has been created."}
